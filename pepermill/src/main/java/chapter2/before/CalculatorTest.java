df05cac45f3d98467b7aaac6f72564cbb4bd35d6
namuhuchutong <namuhuchutong@gmail.com>
2023-01-11 19:04:52 +0900

    Feat: 2장 연습 - 자동화 테스트 없이 구현

---
+++ pepermill/src/main/java/chapter2/before/CalculatorTest.java
@@ -0,0 +1,16 @@
+package chapter2.before;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+
+		int number1 = 20;
+		int number2 = 10;
+
+		System.out.println(calculator);
+		System.out.println(calculator);
+		System.out.println(calculator);
+		System.out.println(calculator);
+	}
+}
