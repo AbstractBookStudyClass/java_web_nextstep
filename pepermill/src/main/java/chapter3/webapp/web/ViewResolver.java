990c6ba8f0890b1bc5b035526d7ab91e30bab17c
namuhuchutong <namuhuchutong@gmail.com>
2023-01-18 20:27:13 +0900

    Feat: View 로직 분리

---
+++ pepermill/src/main/java/chapter3/webapp/web/ViewResolver.java
@@ -0,0 +1,49 @@
+package chapter3.webapp.web;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ViewResolver {
+
+	private static final Logger log = LoggerFactory.getLogger(ViewResolver.class);
+	private static final String RESOURCES_PATH = "src/main/resources/template";
+
+	public byte[] getView(View view) {
+		byte[] header = null;
+		byte[] body = null;
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+		// 실제 html 문서와 헤더를 더해서 전달한다.
+		try {
+			body = getDocument(view);
+			view.setLengthOfBodyContent(body.length);
+			header = View.from(view, view.getHttpStatusCode());
+			log.debug(new String(header));
+			outputStream.write(header);
+			outputStream.write(body);
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			throw new RuntimeException("문서가 존재하지 않습니다");
+		}
+
+		log.debug(new String(outputStream.toByteArray()));
+		return outputStream.toByteArray();
+	}
+
+	private byte[] getDocument(View view) throws IOException {
+		String url;
+		if (view.getUrl() == null) {
+			url = view.getLocation();
+		} else {
+			url = view.getUrl();
+		}
+		Path path = Paths.get(RESOURCES_PATH + url);
+		return Files.readAllBytes(path);
+	}
+}
