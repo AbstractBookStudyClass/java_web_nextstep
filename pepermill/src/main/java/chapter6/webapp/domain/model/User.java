6cc1f1c7267b1a277061812fed5ac84acb86c945
namuhuchutong <namuhuchutong@gmail.com>
2023-02-15 15:39:43 +0900

    refactor: user 모델 식별자 추가

--- pepermill/src/main/java/chapter6/webapp/domain/model/User.java
+++ pepermill/src/main/java/chapter6/webapp/domain/model/User.java
@@ -4,12 +4,27 @@ import java.util.Objects;
 
 public class User {
 
+	private Integer userId;
 	private String userName;
 	private String password;
+
+	public User(final Integer userId, final String userName, final String password) {
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+	}
 
 	public User(final String userName, final String password) {
 		this.userName = userName;
 		this.password = password;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(final Integer userId) {
+		this.userId = userId;
 	}
 
 	public String getUserName() {
@@ -29,25 +44,27 @@ public class User {
 	}
 
 	@Override
-	public String toString() {
-		return "User{" +
-			"userName='" + userName + '\'' +
-			", password='" + password + '\'' +
-			'}';
-	}
-
-	@Override
 	public boolean equals(final Object o) {
 		if (this == o)
 			return true;
 		if (o == null || getClass() != o.getClass())
 			return false;
 		final User user = (User)o;
-		return Objects.equals(userName, user.userName) && Objects.equals(password, user.password);
+		return Objects.equals(userId, user.userId) && Objects.equals(userName, user.userName)
+			&& Objects.equals(password, user.password);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(userName, password);
+		return Objects.hash(userId, userName, password);
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+			"userId=" + userId +
+			", userName='" + userName + '\'' +
+			", password='" + password + '\'' +
+			'}';
 	}
 }
